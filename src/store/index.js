4bef3828212ce85e3c18d02583dd5fae5cc4c174
Bence Varga <Bence.Varga2@ibm.com>
2025-05-22 13:04:43 +0200

    Add Redux store with counterReducer

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'redux'
+
+const counterReducer = (state = { counter : 0 }, action) => {
+    if(action.type === 'INCREMENT') {
+        return{
+            counter: state.counter + 1
+        }
+    } else if (action.type === 'DECREMENT') {
+        return{
+            counter: state.counter - 1
+        }
+    } else {
+        return state;
+    }
+}
+
+const store = createStore(counterReducer)
+
+export default store;
